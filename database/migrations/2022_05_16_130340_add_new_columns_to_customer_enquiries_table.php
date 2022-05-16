26ca8c1bbbf4489410e61c7b72d007eee2d29baa
Maaz <maaz790@gmail.com>
2022-05-16 18:10:17 +0530

    migration changes from pradyuman

---
+++ database/migrations/2022_05_16_130340_add_new_columns_to_customer_enquiries_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddNewColumnsToCustomerEnquiriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('customer_enquiries', function (Blueprint $table) {
+            $table->integer('user_address_id')->default(0)->after('packaging_treatment_id');
+            $table->dropColumn('address');
+            $table->dropColumn('country_id');
+            $table->dropColumn('city_id');
+            $table->dropColumn('state_id');
+            $table->dropColumn('pincode');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('customer_enquiries', function (Blueprint $table) {
+            $table->dropColumn('user_address_id');
+            $table->dropColumn('order_id');
+        });
+    }
+}
