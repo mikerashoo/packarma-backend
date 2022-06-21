eaed80cc8a9bdfaeb05f4257fd6b5daa3be91130
Maaz <maaz790@gmail.com>
2022-06-21 11:50:17 +0530

    View profile and update profile api changes

--- app/Http/Controllers/vendorapi/MyProfileController.php
+++ app/Http/Controllers/vendorapi/MyProfileController.php
@@ -18,6 +18,7 @@ class MyProfileController extends Controller
      */
     public function show(Request $request)
     {
+
         $msg_data = array();
         \Log::info("My Profile Show, starting at: " . Carbon::now()->format('H:i:s:u'));
         try {
@@ -26,7 +27,7 @@ class MyProfileController extends Controller
                 $vendor_id = $vendor_token['sub'];
 
 
-                $data = Vendor::select('vendor_name', 'vendor_company_name', 'vendor_email', 'phone', 'whatsapp_no', 'phone_country_id', 'whatsapp_country_id')
+                $data = Vendor::select('vendor_name', 'vendor_company_name', 'vendor_email', 'phone', 'whatsapp_no', 'phone_country_id', 'whatsapp_country_id', 'approval_status')
                     ->with(['phone_country' => function ($query) {
                         $query->select('id', 'country_name', 'phone_code');
                     }])
@@ -37,17 +38,33 @@ class MyProfileController extends Controller
                 if (empty($data)) {
                     errorMessage(__('my_profile.not_found'), $msg_data);
                 }
+                if ($data->approval_status != 'accepted') {
+                    $flags = array(
+                        "my_address" => false,
+                        "change_password" => false,
+                        "about_us" => false,
+                        "help_and_support" => false,
+                        "terms_and_condition" => false,
+                        "privacy_policy" => false,
+                        "edit_vendor" => false,
+                        "delete_vendor" => false,
+                        "logout" => true,
+                    );
+                } else {
+                    $flags = array(
+                        "my_address" => true,
+                        "change_password" => true,
+                        "about_us" => true,
+                        "help_and_support" => true,
+                        "terms_and_condition" => true,
+                        "privacy_policy" => true,
+                        "edit_vendor" => true,
+                        "delete_vendor" => true,
+                        "logout" => true,
+                    );
+                }
 
-                $flags = array(
-                    "my_address" => true,
-                    "change_password" => true,
-                    "about_us" => true,
-                    "help_and_support" => true,
-                    "terms_and_condition" => true,
-                    "privacy_policy" => true,
-                    "edit_vendor" => true,
-                    "delete_vendor" => true,
-                );
+
 
                 $msg_data['result'] = $data;
                 $msg_data['flags'] = $flags;
@@ -86,18 +103,29 @@ class MyProfileController extends Controller
                 }
                 \Log::info("Vendor Update Start!");
 
-                if (isset($request->vendor_email) && !empty($request->vendor_email)) {
+
+
+                $checkVendor = Vendor::select('approval_status', 'vendor_email', 'phone', 'status')->where([['id', $vendor_id]])->first();
+
+                if (isset($request->vendor_email) && (strtolower($request->vendor_email) != strtolower($checkVendor->vendor_email))) {
                     errorMessage(__('vendor.email_cant_update'), $msg_data);
                 }
 
-                if (isset($request->phone) && !empty($request->phone)) {
+                if (isset($request->phone) && ($request->phone != $checkVendor->phone)) {
                     errorMessage(__('vendor.phone_cant_update'), $msg_data);
                 }
 
 
-                $checkVendor = Vendor::where([['phone', $request->phone], ['id', '!=', $vendor_id]])->first();
-                if (!empty($checkVendor)) {
-                    errorMessage(__('vendor.same_phone_exist'), $msg_data);
+                if ($checkVendor->approval_status == 'rejected') {
+                    errorMessage(__('vendor.rejected'), $msg_data);
+                }
+
+                if ($checkVendor->approval_status == 'pending') {
+                    errorMessage(__('vendor.approval_pending'), $msg_data);
+                }
+
+                if ($checkVendor->status == 0 && $checkVendor->approval_status == 'accepted') {
+                    errorMessage(__('vendor.not_active'), $msg_data);
                 }
 
 
@@ -150,9 +178,9 @@ class MyProfileController extends Controller
         return \Validator::make($request->all(), [
             'vendor_name' => 'required|string',
             'vendor_company_name' => 'required|string',
-            // 'phone_country_id' => 'required|numeric',
-            // 'phone' => 'required|numeric',
-            // 'vendor_email' => 'required|email',
+            'phone_country_id' => 'required|numeric',
+            'phone' => 'required|numeric|digits:10',
+            'vendor_email' => 'required|email',
 
         ])->errors();
     }
