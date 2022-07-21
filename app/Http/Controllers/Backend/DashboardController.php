d5e646ba72213274820955c41a27b93009760085
pradyumndwivedi <pradyumndwivedi11@gmail.com>
2022-07-21 14:32:31 +0530

    Modified dashboard count.

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -29,16 +29,17 @@ class DashboardController extends Controller
 		// exit;
 
 		$data['user'] = User::where('approval_status','=','accepted')->count(); 
-		$data['cust_reg_today'] = User::where('approval_status','=','accepted')->whereDate('approved_on','=', date('Y-m-d'))->count();
+		// $data['user'] = User::all()->count(); 
+		$data['cust_reg_today'] = User::whereDate('created_at','=', date('Y-m-d'))->count();
 		$data['vendor'] = Vendor::where('approval_status','=','accepted')->count(); 
-		$data['vendor_reg_today'] = Vendor::where('approval_status','=','accepted')->whereDate('approved_on','=', date('Y-m-d'))->count();
+		$data['vendor_reg_today'] = Vendor::whereDate('created_at','=', date('Y-m-d'))->count();
 		$data['revenue'] = Order::all()->sum('grand_total');
 		$data['today_sales'] = Order::whereDate('created_at','=', date('Y-m-d'))->count();
 		$data['subs_renew_today'] = User::whereDate('subscription_start','=', date('Y-m-d'))->count();
 		$data['subs_end_today'] = User::whereDate('subscription_end','=', date('Y-m-d'))->count();
-		$data['today_orders'] = Order::whereDate('created_at','=', date('Y-m-d'))->count();
+		$data['total_orders'] = Order::all()->count();
 		$data['pending_orders'] = Order::where('order_delivery_status','=','pending')->count();
-		$data['processing_orders'] = Order::where('order_delivery_status','=','processing')->orWhere('order_delivery_status','=','out_for_delivery')->count();
+		$data['processing_orders'] = Order::whereIn('order_delivery_status', ['processing', 'out_for_delivery'])->count();
 		$data['completed_orders'] = Order::where('order_delivery_status','=','delivered')->count();  
     	return view('backend/dashboard/index',["data"=>$data]);
 	}
