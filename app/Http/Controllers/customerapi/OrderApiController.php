a823f330fd2a2cdc4b5bb25a1ac43d072cbc2fbe
pradyumndwivedi <pradyumndwivedi11@gmail.com>
2022-07-21 15:26:50 +0530

    minor change in order api.

--- app/Http/Controllers/customerapi/OrderApiController.php
+++ app/Http/Controllers/customerapi/OrderApiController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Models\OrderPayment;
-use App\Models\Customerenquiry;
+use App\Models\CustomerEnquiry;
 use App\Models\Order;
 use App\Models\UserAddress;
 use App\Models\VendorQuotation;
