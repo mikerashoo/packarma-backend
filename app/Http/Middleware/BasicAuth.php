bd6aedd4086e42bd2cc9ba9003e6b415b3c04a83
Maaz <maaz790@gmail.com>
2022-08-30 11:50:37 +0530

    imei issue on customerapi

--- app/Http/Middleware/BasicAuth.php
+++ app/Http/Middleware/BasicAuth.php
@@ -9,12 +9,12 @@ use Illuminate\Http\Request;
 class BasicAuth
 {
     /**
-    * Handle an incoming request.
-    *
-    * @param  \Illuminate\Http\Request  $request
-    * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
-    * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
-    */
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
     public function handle(Request $request, Closure $next)
     {
         $return_array = array();
@@ -56,8 +56,8 @@ class BasicAuth
         //     exit;
         // }
 
-        if (!$request->header('device-id')) {
-            errorMessage(__('auth.device_id_require'), $return_array);
+        if (!$request->header('imei-no')) {
+            errorMessage(__('auth.imei_require'), $return_array);
             exit;
         }
 
