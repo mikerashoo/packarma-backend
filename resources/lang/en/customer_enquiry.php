e6f5e25d26e1e43062c8594d951cb653b8480a22
pradyumndwivedi <pradyumndwivedi11@gmail.com>
2022-09-30 18:39:59 +0530

    minor change in customer enquiry store for product

--- resources/lang/en/customer_enquiry.php
+++ resources/lang/en/customer_enquiry.php
@@ -16,4 +16,6 @@ return [
     'customer_enquiry_not_found' => 'Customer Enquiry not found',
     'customer_enquiry_placed_successfully' => 'Customer Enquiry Placed Successfully',
     'product_quantity_should_be_greater_than_minimum_order_quantity' => 'Product quantity should be greater than minimum order quantity for selected packaging solution',
+    'product_weight_should_be_in_between_min_and_max_weight' => 'Product weight should be in between minimum and maximum weight for selected packaging solution',
+
 ];
