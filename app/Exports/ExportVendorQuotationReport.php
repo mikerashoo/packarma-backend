3f2700f336815d0145efe558183affefccdd5e6d
sway2050 <swayama2050@gmail.com>
2023-01-13 19:46:18 +0530

    added reports section in webadmin and moved storage to s3

---
+++ app/Exports/ExportVendorQuotationReport.php
@@ -0,0 +1,215 @@
+<?php
+
+namespace App\Exports;
+
+use App\Models\VendorQuotation;
+use App\Models\VendorWarehouse;
+use Carbon\Carbon;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithCustomStartCell;
+use Maatwebsite\Excel\Events\AfterSheet;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use PhpOffice\PhpSpreadsheet\Style\Border;
+use PhpOffice\PhpSpreadsheet\Style\Fill;
+use Maatwebsite\Excel\Concerns\WithTitle;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
+
+
+class ExportVendorQuotationReport implements FromCollection, WithHeadings, WithCustomStartCell, WithEvents, WithTitle, WithStyles
+{
+    function __construct($request)
+    {
+        $this->request = $request;
+    }
+    public function styles(Worksheet $sheet)
+    {
+        return [
+            'A1'    => ['alignment' => ['wrapText' => true]],
+        ];
+    }
+    public function startCell(): string
+    {
+        return 'A2';
+    }
+    public function registerEvents(): array
+    {
+
+        return [
+            AfterSheet::class => function (AfterSheet $event) {
+                /** @var Sheet $sheet */
+                $sheet = $event->sheet;
+                $start_date = Carbon::parse(now())->format('Y-m-d');
+                $end_date = Carbon::parse(now())->format('Y-m-d');
+                if (isset($this->request->daterange) && !empty($this->request->daterange)) {
+                    $string = explode('-', $this->request->daterange);
+                    $start_date = Carbon::createFromFormat('d/m/Y', trim($string[0]))->format('d/m/Y');
+                    $end_date = Carbon::createFromFormat('d/m/Y', trim($string[1]))->format('d/m/Y');
+                }
+                $sheet->mergeCells('A1:T1');
+                $sheet->setCellValue('A1', "Vendor Quotation Based Report (" . $start_date .'-'.$end_date. ')');// . " - " . $end_date . ")");
+
+
+                $styleArray = [
+                    'alignment' => [
+                        'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER,
+                    ],
+                ];
+
+
+
+                $headerFontArray = [
+                    'name' => 'Calibri',
+                    'bold' => TRUE,
+                    'italic' => FALSE,
+                    'underline' => \PhpOffice\PhpSpreadsheet\Style\Font::UNDERLINE_NONE,
+                    'strikethrough' => FALSE,
+                    'size' => 16,
+                    'color' => [
+                        'rgb' => 'FF000000'
+                    ]
+                ];
+                $heading = 'A1:T1'; // Main Heading
+                $event->sheet->getDelegate()->getStyle($heading)->applyFromArray($styleArray);
+                $event->sheet->getDelegate()->getStyle($heading)->getFont()->applyFromArray($headerFontArray);
+
+
+                //column style
+                $columnFontArray = [
+                    'name' => 'Calibri',
+                    'bold' => TRUE,
+                    'italic' => FALSE,
+                    'underline' => \PhpOffice\PhpSpreadsheet\Style\Font::UNDERLINE_NONE,
+                    'strikethrough' => FALSE,
+                    'size' => 11,
+                    'color' => [
+                        'rgb' => 'FF000000'
+                    ]
+                ];
+
+                $columnBorderArray =   [
+                    'allBorders' => [
+                        'borderStyle' => Border::BORDER_THIN,
+                        'color' => [
+                            'rgb' => 'FF000000'
+                        ]
+                    ]
+                ];
+
+                $columnColorArray = [
+                    'fillType' => Fill::FILL_GRADIENT_LINEAR,
+                    'rotation' => 0.0,
+                    'startColor' => [
+                        'rgb' => 'FFFFFF00'
+                    ],
+                    'endColor' => [
+                        'argb' => 'FFFFFF00'
+                    ]
+                ];
+
+                $headerColumn = 'A2:T2'; // Columns
+                $columns = range('A', 'T');
+                foreach ($columns as $elements) {
+                    $event->sheet->getDelegate()->getColumnDimension($elements)->setWidth(20);
+                }
+
+                $event->sheet->getDelegate()->getStyle($headerColumn)->getFill()->applyFromArray($columnColorArray);
+                $event->sheet->getDelegate()->getStyle($headerColumn)->getBorders()->applyFromArray($columnBorderArray);
+                $event->sheet->getDelegate()->getStyle($headerColumn)->getFont()->applyFromArray($columnFontArray);
+            },
+        ];
+    }
+    public function headings(): array
+    {
+        return [
+            'Quotation No.',
+            'Customer Enquiry No',	
+            'Vendor Name',	
+            'Vendor Price', 
+            'Product Name', 	
+            'Product Quantity',
+            'MRP',
+            'Sub-Total',
+            'GST Amount',
+            'GST Type',
+            'GST %',
+            'Freight Amount',
+            'Delivery In Days',
+            'Grand Total',
+            'Vendor Amount',
+            'Commission',
+            'Enquiry Status',
+            'Quotation enquiry Datetime',
+            'Lead Time',
+            'Vendor Warehouse Address'
+        ];
+    }
+    /**
+    * @return \Illuminate\Support\Collection
+    */
+    public function collection()
+    {
+        $start_date = Carbon::parse(now())->format('Y-m-d');
+        $end_date = Carbon::parse(now())->format('Y-m-d');
+        if (isset($this->request->daterange) && !empty($this->request->daterange)) {
+            $string = explode('-', $this->request->daterange);
+            $start_date = Carbon::createFromFormat('d/m/Y', trim($string[0]))->startOfDay()->toDateTimeString();
+            $end_date =Carbon::createFromFormat('d/m/Y', trim($string[1]))->endOfDay()->toDateTimeString();
+        }
+        $vendor_quotation = VendorQuotation::with('vendor',
+                                           'vendor_warehouse',
+                                           'product',
+                                           'customer_enquiry')
+                                  ->select('id',
+                                           'customer_enquiry_id',
+                                           'vendor_id',
+                                           'vendor_price',
+                                           'product_id',
+                                           'product_quantity',
+                                           'mrp',
+                                           'sub_total',
+                                           'gst_amount',
+                                           'gst_type',
+                                           'gst_percentage',
+                                           'freight_amount',
+                                           'delivery_in_days',
+                                           'total_amount',
+                                           'vendor_amount',
+                                           'commission',
+                                           'enquiry_status',
+                                           'quotation_expiry_datetime',
+                                           'lead_time',
+                                           'vendor_warehouse_id');
+        if($this->request->vendor_quotation_vendor){
+            if($this->request->vendor_quotation_vendor[0]!='All'){
+                $vendor_ids = $this->request->vendor_quotation_vendor;
+                $vendor_quotation->whereIn('vendor_id',$vendor_ids);
+            }
+            
+        }
+        if($this->request->vendor_quotation_packaging_material){
+            if($this->request->vendor_quotation_packaging_material[0]!='All'){
+                $packaging_material_ids = $this->request->vendor_quotation_packaging_material;
+                $vendor_quotation->whereIn('customer_enquiry.packaging_material_id',$packaging_material_ids);
+            }
+        }
+        $vendor_quotation = $vendor_quotation->whereBetween('created_at',[$start_date,$end_date])
+                                             ->orWhereBetween('updated_at',[$start_date,$end_date])
+                                             ->get();
+        foreach($vendor_quotation as $enquiry){
+            $enquiry->vendor_id =$enquiry->vendor->vendor_name;
+            $enquiry->product_id =  $enquiry->product->product_name;
+            $vendor_warehouse = VendorWarehouse::with('city','state','country')->find($enquiry->vendor_warehouse_id);
+            $enquiry->vendor_warehouse_id =  $vendor_warehouse!=null?$vendor_warehouse->warehouse_name .' '. $vendor_warehouse->flat .','. $vendor_warehouse->area .','. $vendor_warehouse->landmark .','. $vendor_warehouse->city_name .','. $vendor_warehouse->state->state_name .' '. $vendor_warehouse->country->country_name .' '. $vendor_warehouse->pincode:'-';
+            $enquiry->enquiry_status = vendorEnquiryStatus($enquiry->enquiry_status);
+        }
+        return $vendor_quotation;
+    }
+
+    public function title(): string
+    {
+        $sheetName = 'Vendor Quotation Data ' . Carbon::now()->format('Y-m-d');
+        return $sheetName;
+    }
+}
--- app/Http/Helpers/Imagehelper.php
+++ app/Http/Helpers/Imagehelper.php
@@ -2,53 +2,71 @@
 
 use Illuminate\Support\Facades\Storage;
 use Image as thumbimage;
+use Illuminate\Support\Facades\File;
 
 if (!function_exists('getFile')) {
     function getFile($name, $type, $isBanner = "false", $for = "image")
     {
 
+        $expiryDate = now()->addDay(); //The link will be expire after 1 day
         $defaultImagePath = "";
         if ($isBanner) {
-            $defaultImagePath =  URL::to('/') . '/storage/app/public' . '/uploads/default_banner.jpg?d=' . time();
+            // $defaultImagePath =  URL::to('/') . '/storage/app/public' . '/uploads/default_banner.jpg?d=' . time();
+            $src = $type . '/' . 'default_banner.jpg';
+
         } else {
-            $defaultImagePath =  URL::to('/') . '/storage/app/public' . '/uploads/default.jpg?d=' . time();
+            // $defaultImagePath =  URL::to('/') . '/storage/app/public' . '/uploads/default.jpg?d=' . time();
+            $src = 'default.jpg';
+
         }
 
         if ($for == 'thumb') {
-            if (!empty($name) && file_exists(storage_path('app/public') . '/uploads/' . $type . '/' . 'thumb' . '/' . $name)) {
-                return URL::to('/') . '/storage/app/public' . '/uploads/' . $type . '/' . 'thumb' . '/' . $name . '?d=' . time();
+            if (!empty($name) && \Storage::disk('s3')->exists($type . '/' . 'thumb' . '/' . $name)) {
+                // return URL::to('/') . '/storage/app/public' . '/uploads/' . $type . '/' . 'thumb' . '/' . $name . '?d=' . time();
+                 $src = $type . '/' . 'thumb' . '/' . $name;
+                 return \Storage::disk('s3')->temporaryUrl($src, $expiryDate);
+
             } else {
-                return $defaultImagePath;
+                return \Storage::disk('s3')->temporaryUrl($src, $expiryDate);
+              
             }
         } elseif ($for == 'unlink') {
-            if (!empty($name) && file_exists(storage_path('app/public') . '/uploads/' . $type . '/' . $name)) {
-                return storage_path('app/public') . '/uploads/' . $type . '/' . $name;
+            if (!empty($name) && \Storage::disk('s3')->exists($type . '/' . $name)) {
+                $src =  $type . '/' . $name;
+                return \Storage::disk('s3')->temporaryUrl($src, $expiryDate);
+
             } else {
-                return  'file_not_found';
+                return 'file_not_found';
             }
         } elseif ($for == 'front' || $for == 'back') {
-            if (!empty($name) && file_exists(storage_path('app/public') . '/uploads/' . $type . '/' . $name)) {
-                return URL::to('/') . '/storage/app/public' . '/uploads/' . $type . '/' . $name . '?d=' . time();
+            if (!empty($name) && \Storage::disk('s3')->exists($type . '/' . $name)) {
+                $src = $type . '/' . $name . '?d=' . time();
+                return \Storage::disk('s3')->temporaryUrl($src, $expiryDate);
             } else {
                 return  'file_not_found';
             }
         } elseif ($for == 'gst_certificate') {
-            if (!empty($name) && file_exists(storage_path('app/public') . '/uploads/' . $type . '/' . $name)) {
-                return URL::to('/') . '/storage/app/public' . '/uploads/' . $type . '/' . $name . '?d=' . time();
+            if (!empty($name) && \Storage::disk('s3')->exists($type . '/' . $name)) {
+                $src = $type . '/' . $name;
+                return \Storage::disk('s3')->temporaryUrl($src, $expiryDate);
             } else {
-                return  URL::to('/') . '/storage/app/public' . '/uploads/' . $type . '/default_user_gst_file.png?d=' . time();
+                $src = 'default_user_gst_file.png';
+                return \Storage::disk('s3')->temporaryUrl($src, $expiryDate);
             }
         } elseif ($for == 'vendor_gst_certificate') {
-            if (!empty($name) && file_exists(storage_path('app/public') . '/uploads/' . $type . '/' . $name)) {
-                return URL::to('/') . '/storage/app/public' . '/uploads/' . $type . '/' . $name . '?d=' . time();
+            if (!empty($name) && \Storage::disk('s3')->exists($type . '/' . $name)) {
+                $src = $type . '/' . $name;
+                return \Storage::disk('s3')->temporaryUrl($src, $expiryDate);
             } else {
-                return  URL::to('/') . '/storage/app/public' . '/uploads/' . $type . '/default_vendor_gst_file.png?d=' . time();
+                $src = 'default_vendor_gst_file.png';
+                return \Storage::disk('s3')->temporaryUrl($src, $expiryDate);
             }
         } else {
-            if (!empty($name) && file_exists(storage_path('app/public') . '/uploads/' . $type . '/' . $name)) {
-                return URL::to('/') . '/storage/app/public' . '/uploads/' . $type . '/' . $name . '?d=' . time();
+            if (!empty($name) && \Storage::disk('s3')->exists($type . '/' . $name)) {
+                $src = $type . '/' . $name;
+                return \Storage::disk('s3')->temporaryUrl($src, $expiryDate);
             } else {
-                return  $defaultImagePath;
+                return \Storage::disk('s3')->temporaryUrl($src, $expiryDate);
             }
         }
     }
@@ -56,39 +74,42 @@ if (!function_exists('getFile')) {
 
 
 if (!function_exists('ListingImageUrl')) {
-    function ListingImageUrl($type, $imageName, $for = "image", $isBanner = "false")
+    function ListingImageUrl($type, $name, $for = "image", $isBanner = "false")
     {
         $src = '';
+        $expiryDate = now()->addDay(); //The link will be expire after 1 day
         $defaultImagePath = "";
         if ($isBanner) {
-            $defaultImagePath =  'storage/app/public/uploads/default_banner.jpg?d=' . time();
+            $defaultImagePath = 'default_banner.jpg';
         } else {
-            $defaultImagePath =  'storage/app/public/uploads/default.jpg?d=' . time();
+            $defaultImagePath =  'default.jpg';
         }
         if ($for == 'thumb') {
-            if (!empty($imageName) && file_exists('storage/app/public/uploads/' . $type . '/' . 'thumb' . '/' . $imageName)) {
-                $src =  'storage/app/public/uploads/' . $type . '/' . 'thumb' . '/' . $imageName . '?d=' . time();
+            
+            if (!empty($name) && \Storage::disk('s3')->exists($type . '/' . 'thumb' . '/' . $name)) {
+                $src = $type . '/' . 'thumb' . '/' . $name;
             } else {
                 //default image path
-                $src = $defaultImagePath;
+                $src = $type . '/' .$defaultImagePath;
             }
         } else {
-            if (!empty($imageName) && file_exists('storage/app/public/uploads/' . $type . '/' . $imageName)) {
-                $src =  'storage/app/public/uploads/' . $type . '/' . $imageName . '?d=' . time();
+            if (!empty($name) && \Storage::disk('s3')->exists($type . '/' . $name)) {
+                $src = $type . '/' . $name;
             } else {
                 //default image path
-                $src = $defaultImagePath;
+                $src = $type . '/' .$defaultImagePath;
             }
         }
-        return url($src);
+        // return url($src);
+        return \Storage::disk('s3')->temporaryUrl($src, $expiryDate);
+
     }
 }
-
 
 if (!function_exists('file_exist_ret')) {
     function file_exist_ret($type, $id, $ib, $ext)
     {
-        if (file_exists('storage/app/public/uploads/' . $type . '/' . $type . '_' . $id . '_' . $ib . '.' . $ext)) {
+        if (file_exists(\Storage::disk('s3')->exists($type . '/' . $type . '_' . $id . '_' . $ib . '.' . $ext))) {
             $ib = $ib + 1;
             return file_exist_ret($type, $id, $ib, $ext);
         } else {
@@ -97,36 +118,36 @@ if (!function_exists('file_exist_ret')) {
     }
 }
 
-if (!function_exists('file_view')) {
-    function file_view($type, $id, $num_of_imgs)
+//if (!function_exists('file_view')) {
+//    function file_view($type, $id, $num_of_imgs)
     {
-        $files = explode(',', implode(',', preg_grep('~^' . $type . '_' . $id . '_*~', scandir("storage/app/public/uploads/" . $type))));
-        $src = '';
-        $src_array = array();
-        if ($files[0] != '') {
-            for ($i = 0; $i < count($files); $i++) {
-                if ($src == '') {
-                    $src = 'storage/app/public/uploads/' . $type . '/' . $files[$i] . '||' . str_replace($type . '_', '', (explode('.', $files[$i]))[0]);
-                    array_push($src_array, url($src));
-                } else {
-                    $src = 'storage/app/public/uploads/' . $type . '/' . $files[$i] . '||' . str_replace($type . '_', '', (explode('.', $files[$i]))[0]);
-                    array_push($src_array, url($src));
-                }
-            }
-        }
-        return $src_array;
-    }
-}
+//        $files = explode(',', implode(',', preg_grep('~^' . $type . '_' . $id . '_*~', scandir("storage/app/public/uploads/" . $type))));
+//        $src = '';
+//        $src_array = array();
+//        if ($files[0] != '') {
+//            for ($i = 0; $i < count($files); $i++) {
+//                if ($src == '') {
+//                    $src = 'storage/app/public/uploads/' . $type . '/' . $files[$i] . '||' . str_replace($type . '_', '', (explode('.', $files[$i]))[0]);
+                    	    	
+//		    array_push($src_array, url($src));
+//                } else {
+//                    $src = 'storage/app/public/uploads/' . $type . '/' . $files[$i] . '||' . str_replace($type . '_', '', (explode('.', $files[$i]))[0]);
+//                    array_push($src_array, url($src));
+//                }
+//            }
+//        }
+//        return $src_array;
+//    }
+//}
 
 
 if (!function_exists('saveSingleImage')) {
     function saveSingleImage($file, $type = "", $id = "")
     {
-        //$image = $request->file('attachment');
-        $actualImagePath = 'uploads/' . $type;
+        $actualImagePath = $type;
         $extension = $file->extension();
         $originalImageName = $type . '_' . $id . '.' . $extension;
-        $file->storeAs($actualImagePath, $originalImageName, 'public');
+        \Storage::disk("s3")->putFileAs($actualImagePath, $file, $originalImageName);
         return $originalImageName;
     }
 }
@@ -134,20 +155,45 @@ if (!function_exists('saveSingleImage')) {
 if (!function_exists('createThumbnail')) {
     function createThumbnail($file, $type = "", $id = "", $for = "image")
     {
-        $extension = $file->extension();
-        $originalThumbImageName = $type . '_thumb_' . $id . '.' . $file->extension();
-        $thumbnailFilePath = storage_path('app/public/uploads/' . $type . '/thumb');
-        $img = thumbimage::make($file->path());
-        $width = "100";
-        $height = "100";
+        $avatar = $file;
+        $extension = $file->getClientOriginalExtension();
+        $filename =  $type . '_thumb_' . $id . '.' . $file->extension();
+        $thumbnailFilePath =  $type . '/thumb';
+
+        $width = config('global.DEFAULT_THUMB_IMAGE_WIDTH');
+        $height = config('global.DEFAULT_THUMB_IMAGE_HEIGHT');
         if ($for == 'banner') {
-            $width = "100";
-            $height = "70";
+            $width = config('global.BANNER_THUMB_IMAGE_WIDTH');
+            $height = config('global.BANNER_THUMB_IMAGE_HEIGHT');
         }
-        $img->resize($width, $height, function ($const) {
-            //$const->aspectRatio();
-        })->save($thumbnailFilePath . '/' . $originalThumbImageName);
-        return $originalThumbImageName;
+
+        $normal = thumbimage::make($avatar)->resize($width, $height)->encode($extension);
+        \Storage::disk('s3')->put($thumbnailFilePath . '/' . $filename, (string)$normal);
+
+        return $filename;
+    }
+}
+
+if (!function_exists('createThumbnailMultiple')) {
+    function createThumbnailMultiple($file, $type = "", $id = "", $for = "image",$i = "")
+    {
+
+        $avatar = $file;
+        $extension = $file->getClientOriginalExtension();
+        $filename =  $type . '_thumb_' . $id . '_' . $i  . '.' . $file->extension();
+        $thumbnailFilePath =  $type.'/'.$type . '_' . $id . '/thumb';
+
+        $width = config('global.DEFAULT_THUMB_IMAGE_WIDTH');
+        $height = config('global.DEFAULT_THUMB_IMAGE_HEIGHT');
+        if ($for == 'banner') {
+            $width = config('global.BANNER_THUMB_IMAGE_WIDTH');
+            $height = config('global.BANNER_THUMB_IMAGE_HEIGHT');
+        }
+
+        $normal = thumbimage::make($avatar)->resize($width, $height)->encode($extension);
+        \Storage::disk('s3')->put($thumbnailFilePath . '/' . $filename, (string)$normal);
+
+        return $filename;
     }
 }
 
@@ -155,17 +201,29 @@ if (!function_exists('createThumbnail')) {
 if (!function_exists('file_view')) {
     function file_view($type, $id, $num_of_imgs)
     {
-        $files = explode(',', implode(',', preg_grep('~^' . $type . '_' . $id . '_*~', scandir("storage/app/public/uploads/" . $type))));
+        $expiryDate = now()->addDay(); //The link will be expire after 1 day
+        $src_files = $type.'/'.$type.'_'.$id;
+        $files = \Storage::disk('s3')->allFiles($src_files, $expiryDate);
         $src = '';
-        if ($files[0] != '') {
-            for ($i = 0; $i < count($files); $i++) {
-                if ($src == '') {
-                    $src = 'storage/app/public/uploads/' . $type . '/' . $files[$i] . '||' . str_replace('products_', '', (explode('.', $files[$i]))[0]);
-                } else {
-                    $src = $src . ',storage/app/public/uploads/' . $type . '/' . $files[$i] . '||' . str_replace('products_', '', (explode('.', $files[$i]))[0]);
+        $src_array = array();
+        $i=1;
+        foreach($files as $file) {
+            $ext = pathinfo($file, PATHINFO_EXTENSION);
+            if ($src == '') {
+                if (in_array($type.'/'.$type.'_'.$id.'/'.$type.'_'.$id.'_'.$i.'.'.$ext, $files)){
+                    $src = \Storage::disk('s3')->temporaryUrl($type.'/'.$type.'_'.$id.'/'.$type.'_'.$id.'_'.$i.'.'.$ext, $expiryDate);
+                    array_push($src_array, $src);
+                }
+            } else {
+                if (in_array($type.'/'.$type.'_'.$id.'/'.$type.'_'.$id.'_'.$i.'.'.$ext, $files)){
+                    $src = \Storage::disk('s3')->temporaryUrl($type.'/'.$type.'_'.$id.'/'.$type.'_'.$id.'_'.$i.'.'.$ext, $expiryDate);
+                    array_push($src_array, $src);
                 }
             }
+            $i++;
         }
-        return $src;
+        return $src_array;
     }
 }
+    }
+
