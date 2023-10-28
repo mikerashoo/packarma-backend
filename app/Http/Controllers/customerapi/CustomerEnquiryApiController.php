9ec337cb753a5ffb0219b08f5e577178c87b34a0
Mikiyas Birhanu <37136032+mikerashoo@users.noreply.github.com>
2023-10-28 18:07:54 +0300

    packaging_solution_id added to enquery result

--- app/Http/Controllers/customerapi/CustomerEnquiryApiController.php
+++ app/Http/Controllers/customerapi/CustomerEnquiryApiController.php
@@ -106,9 +106,9 @@ class CustomerEnquiryApiController extends Controller
                         $enquery->entered_shelf_life_unit = null;
                     }
 
-                    $recommendationEngines = $enquery->recommendationEngines()->select(['engine_name', 'structure_type', 'display_shelf_life', 'min_order_quantity', 'min_order_quantity_unit'])->get();
+                    $recommendationEngines = $enquery->recommendationEngines()->select(['recommendation_id as packaging_solution_id', 'engine_name', 'structure_type', 'display_shelf_life', 'min_order_quantity', 'min_order_quantity_unit'])->get();
                     if (count($recommendationEngines) == 0) {
-                        $recommendationEngines = RecommendationEngine::where('id', $enquery->recommendation_engine_id)->select(['engine_name', 'structure_type', 'display_shelf_life', 'min_order_quantity', 'min_order_quantity_unit'])->get();
+                        $recommendationEngines = RecommendationEngine::where('id', $enquery->recommendation_engine_id)->select(['id as packaging_solution_id', 'engine_name', 'structure_type', 'display_shelf_life', 'min_order_quantity', 'min_order_quantity_unit'])->get();
                     }
 
                     $enquery->recommendationEngines  = $recommendationEngines;
