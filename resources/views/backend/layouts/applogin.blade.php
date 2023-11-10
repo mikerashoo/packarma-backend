74bb85f5a8ce2d265a3d26c7c8e0be55799b5506
Mikiyas Birhanu <37136032+mikerashoo@users.noreply.github.com>
2023-11-10 12:24:16 +0300

    Login UI issue fix

--- resources/views/backend/layouts/applogin.blade.php
+++ resources/views/backend/layouts/applogin.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html class="loading" lang="en">
+
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,29 +8,33 @@
     <meta name="author" content="MYPCOTINFOTECH">
     <link rel="shortcut icon" type="image/x-icon" href="public/backend/img/ico/favicon.png">
     <title>@yield('title')</title>
-    <link href="https://fonts.googleapis.com/css?family=Rubik:300,400,500,700,900%7CMontserrat:300,400,500,600,700,800,900" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="{{asset('public/backend/fonts/feather/style.min.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset('public/backend/fonts/simple-line-icons/style.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{url('public/backend/fonts/font-awesome/css/font-awesome.min.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{url('public/backend/vendors/css/perfect-scrollbar.min.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{url('public/backend/vendors/css/prism.min.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{url('public/backend/css/bootstrap.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{url('public/backend/css/bootstrap-extended.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{url('public/backend/css/colors.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{url('public/backend/css/components.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset('public/backend/css/style.css')}}">
-    <link rel="stylesheet" href="{{url('public/backend/css/pages/authentication.css')}}">
-    <script src="{{url('public/backend/js/jquery-3.2.1.min.js')}}"></script>
-    <script src="{{url('public/backend/vendors/js/core/bootstrap.min.js')}}" type="text/javascript"></script>
+    <link
+        href="https://fonts.googleapis.com/css?family=Rubik:300,400,500,700,900%7CMontserrat:300,400,500,600,700,800,900"
+        rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="/backend/fonts/feather/style.min.css">
+    <link rel="stylesheet" type="text/css" href="/backend/fonts/simple-line-icons/style.css">
+    <link rel="stylesheet" type="text/css" href="/backend/fonts/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="/backend/vendors/css/perfect-scrollbar.min.css">
+    <link rel="stylesheet" type="text/css" href="/backend/vendors/css/prism.min.css">
+    <link rel="stylesheet" type="text/css" href="/backend/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="/backend/css/bootstrap-extended.css">
+    <link rel="stylesheet" type="text/css" href="/backend/css/colors.css">
+    <link rel="stylesheet" type="text/css" href="/backend/css/components.css">
+    <link rel="stylesheet" type="text/css" href="/backend/css/style.css">
+    <link rel="stylesheet" href="/backend/css/pages/authentication.css">
+    <script src="/backend/js/jquery-3.2.1.min.js"></script>
+    <script src="/backend/vendors/js/core/bootstrap.min.js" type="text/javascript"></script>
 </head>
 
-<body class="vertical-layout vertical-menu 1-column auth-page navbar-sticky blank-page" data-menu="vertical-menu" data-col="1-column">
+<body class="vertical-layout vertical-menu 1-column auth-page navbar-sticky blank-page" data-menu="vertical-menu"
+    data-col="1-column">
     @yield('content')
 </body>
-<script src="{{url('public/backend/js/sidebar.js')}}"></script>
-<script src="{{url('public/backend/vendors/js/switchery.min.js')}}"></script>
-<script src="{{url('public/backend/js/notification-sidebar.js')}}"></script>
-<script src="{{url('public/backend/js/customizer.js')}}"></script>
-<script src="{{url('public/backend/js/scroll-top.js')}}"></script>
-<script src="{{url('public/backend/js/scripts.js')}}"></script>
+<script src="/backend/js/sidebar.js"></script>
+<script src="/backend/vendors/js/switchery.min.js"></script>
+<script src="/backend/js/notification-sidebar.js"></script>
+<script src="/backend/js/customizer.js"></script>
+<script src="/backend/js/scroll-top.js"></script>
+<script src="/backend/js/scripts.js"></script>
+
 </html>
