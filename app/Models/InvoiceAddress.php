cd080ad561c43a86c84a04933cd1bccf98545be6
Mikiyas Birhanu <37136032+mikerashoo@users.noreply.github.com>
2023-11-11 00:37:42 +0300

    Removing softdelete from invoice_address

--- app/Models/InvoiceAddress.php
+++ app/Models/InvoiceAddress.php
@@ -4,13 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class InvoiceAddress extends Model
 {
     use HasFactory;
-    use SoftDeletes;
-    protected $dates = ['deleted_at'];
 
     /**
      * Created By : Pradyumn Dwivedi
